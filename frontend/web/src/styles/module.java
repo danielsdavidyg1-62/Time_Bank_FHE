14e137b0120c330b457bc581cb72315a9c74b2a6
danielsdavidyg1-62 <r-yustitckaya599912959@rambler.ru>
2025-12-05 09:45:41 +0800

    add module.java, handler-simple.rs, IKMSVerifier__factory.ts

---
+++ frontend/web/src/styles/module.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\styles\module.java
+// Generated at: 2025-10-24T13:49:29.600Z
---
+++ handler-simple.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\handler-simple.rs
+// Generated at: 2025-10-24T13:49:29.591Z
